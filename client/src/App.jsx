b78920f8a765c2a4f8d3e7874b0ac30bf5741c77
linoykaufman-dot <linoykaufman@gmail.com>
2026-06-23 20:56:44 +0300

    Update UI and add charts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import "./App.css";
+import MonthlyChart from "./components/MonthlyChart";
+import DashboardCards from "./components/DashboardCards";
+import CategoryPieChart from "./components/CategoryPieChart";
 
 function App() {
+  const currentMonth = new Date().toISOString().slice(0, 7);
   const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
-  const [expenses, setExpenses] = useState([]);
+  const [selectedMonth, setSelectedMonth] = useState(currentMonth);
 
+  const [monthlyExpenses, setMonthlyExpenses] = useState(() => {
+    const saved = localStorage.getItem("monthlyExpenses");
+    return saved ? JSON.parse(saved) : {};
+  });
+
+  const expenses = monthlyExpenses[selectedMonth] || [];
+
+  useEffect(() => {
+    localStorage.setItem(
+      "monthlyExpenses",
+      JSON.stringify(monthlyExpenses)
+    );
+  }, [monthlyExpenses]);
+
+  const totalExpenses = expenses.reduce(
+    (sum, expense) => sum + Number(expense.amount),
+    0
+  );
+const formatMonth = (month) => {
+  const [year, monthNumber] = month.split("-");
+
+  return new Date(year, monthNumber - 1).toLocaleDateString("en-US", {
+    month: "long",
+    year: "numeric",
+  });
+};
+
+const monthlySummary = Object.entries(monthlyExpenses).map(
+  ([month, expenses]) => {
+    const total = expenses.reduce(
+      (sum, expense) => sum + Number(expense.amount),
+      0
+    );
+
+    return {
+      month,
+      total,
+    };
+  }
+);
   const addExpense = () => {
     if (!amount || !category) return;
 
@@ -14,43 +61,83 @@ function App() {
       category,
     };
 
-    setExpenses([...expenses, newExpense]);
+    setMonthlyExpenses({
+      ...monthlyExpenses,
+      [selectedMonth]: [...expenses, newExpense],
+    });
 
     setAmount("");
     setCategory("");
   };
 
+  const deleteExpense = (id) => {
+    setMonthlyExpenses({
+      ...monthlyExpenses,
+      [selectedMonth]: expenses.filter((expense) => expense.id !== id),
+    });
+  };
+
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>Personal Expenses Manager</h1>
+    <div className="app">
+      <div className="card header">
+        <h1>Personal Expenses Manager</h1>
+        <p>Track your monthly spending</p>
 
-      <input
-        type="number"
-        placeholder="Amount"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-      />
+        <input
+          type="month"
+          value={selectedMonth}
+          onChange={(e) => setSelectedMonth(e.target.value)}
+        />
 
-      <input
-        type="text"
-        placeholder="Category"
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-      />
+        <div className="total">₪{totalExpenses}</div>
+      </div>
+      <div className="card">
+  <DashboardCards
+    totalExpenses={totalExpenses}
+    expenses={expenses}
+  />
+</div>
+      <div className="card">
+        <ExpenseForm
+          amount={amount}
+          category={category}
+          setAmount={setAmount}
+          setCategory={setCategory}
+          addExpense={addExpense}
+        />
+      </div>
 
-      <button onClick={addExpense}>
-        Add Expense
-      </button>
+      <div className="card">
+        <ExpenseList
+          expenses={expenses}
+          deleteExpense={deleteExpense}
+        />
+      </div>
+            <div className="card">
+  <h2>Monthly Chart</h2>
+  <MonthlyChart monthlySummary={monthlySummary} />
+</div>
+<div className="card">
+  <h2>Expenses By Category</h2>
 
-      <hr />
+  <CategoryPieChart
+    expenses={expenses}
+  />
+</div>
+      <div className="card">
+  <h2>Monthly History</h2>
+  {monthlySummary.map((item) => (
+    <div className="expense-item" key={item.month}>
+      <div className="expense-info">
+        <span className="amount">{formatMonth(item.month)}</span>
+        <span className="category">Total monthly expenses</span>
+      </div>
+ 
 
-      <h2>Expenses</h2>
-
-      {expenses.map((expense) => (
-        <div key={expense.id}>
-          ₪{expense.amount} - {expense.category}
-        </div>
-      ))}
+      <strong>₪{item.total}</strong>
+    </div>
+  ))}
+</div>
     </div>
   );
 }
